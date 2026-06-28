ac43adbaeed8c5db92d1f88f768b553c142b1c37
Amirul Irsyad <amirulirsyad157@gmail.com>
2026-06-28 08:12:11 +0800

    Added CSRF token requirement

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import CSRFProtect
 import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
+csrf = CSRFProtect(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
